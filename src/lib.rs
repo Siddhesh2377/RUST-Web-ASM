b92eaab339c0d17606fefe130fce45f841a9d5ed
home <darkwildhacker@gmail.com>
2025-03-07 22:30:10 +0530

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 use wasm_bindgen::prelude::*;
 
-// Expose a simple function to JavaScript
 #[wasm_bindgen]
 pub fn greet(name: &str) -> String {
     format!("Hello, {}! Greetings from Rust + WebAssembly.", name)
